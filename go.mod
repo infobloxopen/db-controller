2125287e02c8965d8ba8ea66e99fe0a0f388acff
Andrei Lavrov <alavrov@infoblox.com>
2021-03-30 09:53:00 +0300

    Add db-controller configuration configMap (#4)
    * adding sample charts
    
    * adding configuration configMap to db-controller
    
    * implementing config handling logic
    
    * added config package
    
    * updated deployment
    
    * added UserUpdatedAt to DatabaseClaimConnectionInfo
    
    * enable docker image push
    
    * regenerating CRD

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/infobloxopen/db-controller
 go 1.15
 
 require (
+	github.com/fsnotify/fsnotify v1.4.7
 	github.com/go-logr/logr v0.1.0
 	github.com/google/uuid v1.1.1
 	github.com/lib/pq v1.9.0
 	github.com/onsi/ginkgo v1.11.0
 	github.com/onsi/gomega v1.8.1
+	github.com/spf13/viper v1.7.1
 	k8s.io/apimachinery v0.17.2
 	k8s.io/client-go v0.17.2
 	sigs.k8s.io/controller-runtime v0.5.0
---
+++ pkg/config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/go-logr/logr"
+	"github.com/spf13/viper"
+)
+
+func NewConfig(logger logr.Logger, configFile string) *viper.Viper {
+	c := viper.NewWithOptions(viper.KeyDelimiter("::"))
+	c.SetDefault("config", "config.yaml")
+	c.SetConfigFile(configFile)
+	logger.Info("loading config")
+	err := c.ReadInConfig() // Find and read the config file
+	if err != nil {         // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	// monitor the changes in the config file
+	fmt.Println(viper.AllSettings())
+	c.WatchConfig()
+	c.OnConfigChange(func(e fsnotify.Event) {
+		logger.Info("Config file changed", "file", e.Name)
+		logger.Info(fmt.Sprint(c.AllSettings()))
+	})
+
+	return c
+}
