635b8250c9c916e4dbdaa315128d1548ec53e8be
alavrovinfb <alavrov@infoblox.com>
2021-04-13 01:20:30 +0300

    db controller initial integration tests

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,13 +17,19 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
 	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -41,6 +47,25 @@ var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
 
+var controllerConfig = []byte(`
+    passwordConfig:
+      passwordComplexity: enabled
+      minPasswordLength: "15"
+      passwordRotationPeriod: "60"
+    sample-connection:
+      username: postgres
+      host: localhost
+      port: 5432
+      useSSL: false
+      passwordSecretRef: sample-master-secret
+    another.connection:
+      username: root
+      host: some.other.service
+      port: 5412
+      useSSL: false
+      passwordSecretRef: another-connection-secret
+`)
+
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
@@ -50,7 +75,13 @@ func TestAPIs(t *testing.T) {
 }
 
 var _ = BeforeSuite(func(done Done) {
-	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
+	const (
+		MasterSecretName = "sample-master-secret"
+		Namespace        = "default"
+		timeout          = time.Second * 10
+		interval         = time.Millisecond * 250
+	)
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
@@ -71,6 +102,45 @@ var _ = BeforeSuite(func(done Done) {
 	Expect(err).ToNot(HaveOccurred())
 	Expect(k8sClient).ToNot(BeNil())
 
+	ctx := context.Background()
+	masterSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: Namespace,
+			Name:      MasterSecretName,
+		},
+		Type: "Opaque",
+		Data: map[string][]byte{
+			"Password": []byte("postgres"),
+		},
+	}
+	Expect(k8sClient.Create(ctx, masterSecret)).Should(Succeed())
+
+	secretLookupKey := types.NamespacedName{Name: MasterSecretName, Namespace: Namespace}
+	createdSecret := &corev1.Secret{}
+
+	err = k8sClient.Get(ctx, secretLookupKey, createdSecret)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(createdSecret.Name).Should(Equal(MasterSecretName))
+
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&DatabaseClaimReconciler{
+		Client: k8sClient,
+		Scheme: k8sManager.GetScheme(),
+		Log:    ctrl.Log.WithName("controllers").WithName("DB-controller"),
+		Config: NewConfig(controllerConfig),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
+	time.Sleep(10 * time.Second)
 	close(done)
 }, 60)
 
