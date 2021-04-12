4feb1d861db9657708bb5ec589d815537fcffde4
Andrei Lavrov <alavrov@infoblox.com>
2021-04-12 22:05:59 +0300

    Unit tests (#11)
    * adding unit tests
    
    * added controller unit tests
    
    * move DB logic to separate package

--- controllers/databaseclaim_controller_test.go
+++ controllers/databaseclaim_controller_test.go
@@ -2,12 +2,19 @@ package controllers
 
 import (
 	"bytes"
+	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/spf13/viper"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	persistancev1 "github.com/infobloxopen/db-controller/api/v1"
 )
 
 var complexityEnabled = []byte(`
@@ -24,7 +31,7 @@ var complexityDisabled = []byte(`
       passwordRotationPeriod: "60"
 `)
 
-func TestDatabaseClaimReconciler_generatePassword(t *testing.T) {
+func TestDatabaseClaimReconcilerGeneratePassword(t *testing.T) {
 	type reconciler struct {
 		Client client.Client
 		Log    logr.Logger
@@ -80,4 +87,613 @@ func NewConfig(in []byte) *viper.Viper {
 	c.ReadConfig(bytes.NewBuffer(in))
 
 	return c
+}
+
+type mockClient struct {
+	client.Reader
+	client.Writer
+	client.StatusClient
+}
+
+func (m mockClient) Get(ctx context.Context, key client.ObjectKey, obj runtime.Object) error {
+	_ = ctx
+	if key.Namespace == "testNamespace" && key.Name == "sample-master-secret" {
+		sec, ok := obj.(*corev1.Secret)
+		if !ok {
+			return fmt.Errorf("can't assert type")
+		}
+		sec.Data = map[string][]byte{
+			"password": []byte("masterpassword"),
+		}
+		return nil
+	}
+
+	return fmt.Errorf("not found")
+}
+
+func TestDatabaseClaimReconcilerReadMasterPassword(t *testing.T) {
+	type mockReconciler struct {
+		Client client.Client
+		Log    logr.Logger
+		Scheme *runtime.Scheme
+		Config *viper.Viper
+	}
+	type args struct {
+		ctx         context.Context
+		fragmentKey string
+		namespace   string
+	}
+	tests := []struct {
+		name       string
+		reconciler mockReconciler
+		args       args
+		want       string
+		wantErr    bool
+	}{
+		{
+			"Get master password ok",
+			mockReconciler{
+				Client: &mockClient{},
+				Config: NewConfig(secretRef),
+			},
+			args{
+				fragmentKey: "sample-connection",
+				namespace:   "testNamespace",
+			},
+			"masterpassword",
+			false,
+		},
+		{
+			"Get master password no secret name",
+			mockReconciler{
+				Client: &mockClient{},
+				Config: NewConfig(secretRef),
+			},
+			args{
+				fragmentKey: "",
+				namespace:   "testNamespace",
+			},
+			"",
+			true,
+		},
+		{
+			"Get master password secret not found",
+			mockReconciler{
+				Client: &mockClient{},
+				Config: NewConfig(secretRef),
+			},
+			args{
+				fragmentKey: "secretNameNotExists",
+				namespace:   "testNamespace",
+			},
+			"",
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DatabaseClaimReconciler{
+				Client: tt.reconciler.Client,
+				Log:    tt.reconciler.Log,
+				Scheme: tt.reconciler.Scheme,
+				Config: tt.reconciler.Config,
+			}
+			got, err := r.readMasterPassword(tt.args.ctx, tt.args.fragmentKey, tt.args.namespace)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("readMasterPassword() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("readMasterPassword() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var secretRef = []byte(`
+    sample-connection:
+      PasswordSecretRef: sample-master-secret
+`)
+
+func TestDatabaseClaimReconcilerGetSecretRef(t *testing.T) {
+	type mockReconciler struct {
+		Client client.Client
+		Log    logr.Logger
+		Scheme *runtime.Scheme
+		Config *viper.Viper
+	}
+	type args struct {
+		fragmentKey string
+	}
+	tests := []struct {
+		name       string
+		reconciler mockReconciler
+		args       args
+		want       string
+	}{
+		{
+			"Get master secret reference",
+			mockReconciler{
+				Config: NewConfig(secretRef),
+			},
+			args{fragmentKey: "sample-connection"},
+			"sample-master-secret",
+		},
+		{
+			"Get master secret reference connection does not exist",
+			mockReconciler{
+				Config: NewConfig(secretRef),
+			},
+			args{fragmentKey: "sample-connection111"},
+			"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DatabaseClaimReconciler{
+				Client: tt.reconciler.Client,
+				Log:    tt.reconciler.Log,
+				Scheme: tt.reconciler.Scheme,
+				Config: tt.reconciler.Config,
+			}
+			if got := r.getSecretRef(tt.args.fragmentKey); got != tt.want {
+				t.Errorf("getSecretRef() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var config = []byte(`
+    sample-connection:
+      Username: postgres
+      Host: db-controller-postgresql
+      Port: 5432
+      useSSL: false
+      PasswordSecretRef: sample-master-secret
+`)
+
+func TestDatabaseClaimReconcilerGetConnectionParams(t *testing.T) {
+	type mockReconciler struct {
+		Client client.Client
+		Log    logr.Logger
+		Scheme *runtime.Scheme
+		Config *viper.Viper
+	}
+	type args struct {
+		fragmentKey string
+		dbClaim     *persistancev1.DatabaseClaim
+	}
+	tests := []struct {
+		name       string
+		reconciler mockReconciler
+		args       []args
+		want       []string
+	}{
+		{
+			"Get master connection params Host Port Username",
+			mockReconciler{
+				Config: NewConfig(config),
+			},
+			[]args{
+				{
+					fragmentKey: "sample-connection",
+					dbClaim: &persistancev1.DatabaseClaim{
+						Spec: persistancev1.DatabaseClaimSpec{
+							Host: "",
+						},
+					},
+				},
+				{
+					fragmentKey: "sample-connection",
+					dbClaim: &persistancev1.DatabaseClaim{
+						Spec: persistancev1.DatabaseClaimSpec{
+							Host: "overridden-host",
+						},
+					},
+				},
+				{
+					fragmentKey: "sample-connection",
+					dbClaim: &persistancev1.DatabaseClaim{
+						Spec: persistancev1.DatabaseClaimSpec{
+							Port: "",
+						},
+					},
+				},
+				{
+					fragmentKey: "sample-connection",
+					dbClaim: &persistancev1.DatabaseClaim{
+						Spec: persistancev1.DatabaseClaimSpec{
+							Port: "1234",
+						},
+					},
+				},
+				{
+					fragmentKey: "sample-connection",
+				},
+			},
+			[]string{
+				"db-controller-postgresql",
+				"overridden-host",
+				"5432",
+				"1234",
+				"postgres",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DatabaseClaimReconciler{
+				Client: tt.reconciler.Client,
+				Log:    tt.reconciler.Log,
+				Scheme: tt.reconciler.Scheme,
+				Config: tt.reconciler.Config,
+			}
+			t.Log("getMasterHost() Host from config")
+			if got := r.getMasterHost(tt.args[0].fragmentKey, tt.args[0].dbClaim); got != tt.want[0] {
+				t.Errorf("getMasterHost() = %v, want %v", got, tt.want[0])
+			}
+			t.Log("getMasterHost() Host from config PASS")
+
+			t.Log("getMasterHost() Host overridden by DB claim")
+			if got := r.getMasterHost(tt.args[1].fragmentKey, tt.args[1].dbClaim); got != tt.want[1] {
+				t.Errorf("getMasterPort() = %v, want %v", got, tt.want[1])
+			}
+			t.Log("getMasterPort() Host overridden by DB claim PASS")
+
+			t.Log("getMasterHost() Port from config")
+			if got := r.getMasterPort(tt.args[2].fragmentKey, tt.args[2].dbClaim); got != tt.want[2] {
+				t.Errorf("getMasterPort() = %v, want %v", got, tt.want[2])
+			}
+			t.Log("getMasterPort() Port from config PASS")
+
+			t.Log("getMasterHost() Port overridden by DB claim")
+			if got := r.getMasterPort(tt.args[3].fragmentKey, tt.args[3].dbClaim); got != tt.want[3] {
+				t.Errorf("getMasterPort() = %v, want %v", got, tt.want[3])
+			}
+			t.Log("getMasterPort() Port overridden by DB claim PASS")
+
+			if got := r.getMasterUser(tt.args[4].fragmentKey); got != tt.want[4] {
+				t.Errorf("getMasterUser() = %v, want %v", got, tt.want[4])
+			}
+			t.Log("getMasterUser() PASS")
+		})
+	}
+}
+
+var sslModeDisabled = []byte(`
+    sample-connection:
+      useSSL: false
+`)
+
+var sslModeEnabled = []byte(`
+    sample-connection:
+      useSSL: true
+`)
+
+func TestDatabaseClaimReconcilerGetSSLMode(t *testing.T) {
+	type mockReconciler struct {
+		Client client.Client
+		Log    logr.Logger
+		Scheme *runtime.Scheme
+		Config *viper.Viper
+	}
+	type args struct {
+		fragmentKey string
+	}
+	tests := []struct {
+		name       string
+		reconciler mockReconciler
+		args       args
+		want       string
+	}{
+		{
+			"Get master connection SSL mode",
+			mockReconciler{
+				Config: NewConfig(sslModeDisabled),
+			},
+			args{fragmentKey: "sample-connection"},
+			"disable",
+		},
+		{
+			"Get master connection params Host, Port, user name",
+			mockReconciler{
+				Config: NewConfig(sslModeEnabled),
+			},
+			args{fragmentKey: "sample-connection"},
+			"enable",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DatabaseClaimReconciler{
+				Client: tt.reconciler.Client,
+				Log:    tt.reconciler.Log,
+				Scheme: tt.reconciler.Scheme,
+				Config: tt.reconciler.Config,
+			}
+			if got := r.getSSLMode(tt.args.fragmentKey); got != tt.want {
+				t.Errorf("getSSLMode() = %v, want %v", got, tt.want)
+			}
+			t.Log("getSSLMode() PASS")
+		})
+	}
+}
+
+var multiConfig = []byte(`
+    sample:
+      Host: sample.Host
+    sample.connection:
+      Host: test.Host
+    another.connection:
+      Host: another.Host
+`)
+
+func TestDatabaseClaimReconcilerMatchInstanceLabel(t *testing.T) {
+	type mockReconciler struct {
+		Client client.Client
+		Log    logr.Logger
+		Scheme *runtime.Scheme
+		Config *viper.Viper
+	}
+	type args struct {
+		dbClaim *persistancev1.DatabaseClaim
+	}
+	tests := []struct {
+		name       string
+		reconciler mockReconciler
+		args       args
+		want       string
+		wantErr    bool
+	}{
+		{
+			"Get fragment key",
+			mockReconciler{
+				Config: NewConfig(multiConfig),
+			},
+			args{
+				dbClaim: &persistancev1.DatabaseClaim{
+					Spec: persistancev1.DatabaseClaimSpec{
+						InstanceLabel: "sample.connection",
+					},
+					Status: persistancev1.DatabaseClaimStatus{
+						ConnectionInfo: &persistancev1.DatabaseClaimConnectionInfo{},
+					},
+				},
+			},
+			"sample.connection",
+			false,
+		},
+		{
+			"No key match",
+			mockReconciler{
+				Config: NewConfig(multiConfig),
+			},
+			args{
+				dbClaim: &persistancev1.DatabaseClaim{
+					Spec: persistancev1.DatabaseClaimSpec{
+						InstanceLabel: "blabla",
+					},
+					Status: persistancev1.DatabaseClaimStatus{
+						ConnectionInfo: &persistancev1.DatabaseClaimConnectionInfo{},
+					},
+				},
+			},
+			"",
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DatabaseClaimReconciler{
+				Client: tt.reconciler.Client,
+				Log:    tt.reconciler.Log,
+				Scheme: tt.reconciler.Scheme,
+				Config: tt.reconciler.Config,
+			}
+			got, err := r.matchInstanceLabel(tt.args.dbClaim)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("matchInstanceLabel() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("matchInstanceLabel() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var passwordLength = []byte(`
+    passwordConfig:
+      minPasswordLength: "15"
+`)
+
+func TestDatabaseClaimReconcilerGetMinPasswordLength(t *testing.T) {
+	type mockReconciler struct {
+		Client client.Client
+		Log    logr.Logger
+		Scheme *runtime.Scheme
+		Config *viper.Viper
+	}
+	tests := []struct {
+		name       string
+		reconciler mockReconciler
+		want       int
+	}{
+		{
+			"Get password length",
+			mockReconciler{
+				Config: NewConfig(passwordLength),
+			},
+			15,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DatabaseClaimReconciler{
+				Client: tt.reconciler.Client,
+				Log:    tt.reconciler.Log,
+				Scheme: tt.reconciler.Scheme,
+				Config: tt.reconciler.Config,
+			}
+			if got := r.getMinPasswordLength(); got != tt.want {
+				t.Errorf("getMinPasswordLength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var passwordRotation = []byte(`
+    passwordConfig:
+      passwordRotationPeriod: "75"
+`)
+
+var passwordRotationLess60 = []byte(`
+    passwordConfig:
+      passwordRotationPeriod: "50"
+`)
+
+var passwordRotationGt1440 = []byte(`
+    passwordConfig:
+      passwordRotationPeriod: "2000"
+`)
+
+func TestDatabaseClaimReconcilerGetPasswordRotationTime(t *testing.T) {
+	type mockReconciler struct {
+		Client client.Client
+		Log    logr.Logger
+		Scheme *runtime.Scheme
+		Config *viper.Viper
+	}
+	tests := []struct {
+		name       string
+		reconciler mockReconciler
+		want       time.Duration
+	}{
+		{
+			"Get password rotation time",
+			mockReconciler{
+				Config: NewConfig(passwordRotation),
+				Log:    zap.New(zap.UseDevMode(true)),
+			},
+			75 * time.Minute,
+		},
+		{
+			"Get password rotation time less 60 min",
+			mockReconciler{
+				Config: NewConfig(passwordRotationLess60),
+				Log:    zap.New(zap.UseDevMode(true)),
+			},
+			defaultRotationTime * time.Minute,
+		},
+		{
+			"Get password rotation time greater 1440 min",
+			mockReconciler{
+				Config: NewConfig(passwordRotationLess60),
+				Log:    zap.New(zap.UseDevMode(true)),
+			},
+			defaultRotationTime * time.Minute,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DatabaseClaimReconciler{
+				Client: tt.reconciler.Client,
+				Log:    tt.reconciler.Log,
+				Scheme: tt.reconciler.Scheme,
+				Config: tt.reconciler.Config,
+			}
+			if got := r.getPasswordRotationTime(); got != tt.want {
+				t.Errorf("getPasswordRotationTime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDatabaseClaimReconcilerIsUserChanged(t *testing.T) {
+	type mockReconciler struct {
+		Client client.Client
+		Log    logr.Logger
+		Scheme *runtime.Scheme
+		Config *viper.Viper
+	}
+	type args struct {
+		dbClaim *persistancev1.DatabaseClaim
+	}
+	tests := []struct {
+		name       string
+		reconciler mockReconciler
+		args       args
+		want       bool
+	}{
+		{
+			"User unchanged",
+			mockReconciler{
+				Config: NewConfig(multiConfig),
+			},
+			args{
+				dbClaim: &persistancev1.DatabaseClaim{
+					Spec: persistancev1.DatabaseClaimSpec{
+						Username: "oldUser",
+					},
+					Status: persistancev1.DatabaseClaimStatus{
+						ConnectionInfo: &persistancev1.DatabaseClaimConnectionInfo{
+							Username: "oldUser",
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"User unchanged",
+			mockReconciler{
+				Config: NewConfig(multiConfig),
+			},
+			args{
+				dbClaim: &persistancev1.DatabaseClaim{
+					Spec: persistancev1.DatabaseClaimSpec{
+						Username: "oldUser",
+					},
+					Status: persistancev1.DatabaseClaimStatus{
+						ConnectionInfo: &persistancev1.DatabaseClaimConnectionInfo{
+							Username: "",
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"User changed",
+			mockReconciler{
+				Config: NewConfig(multiConfig),
+			},
+			args{
+				dbClaim: &persistancev1.DatabaseClaim{
+					Spec: persistancev1.DatabaseClaimSpec{
+						Username: "newUser",
+					},
+					Status: persistancev1.DatabaseClaimStatus{
+						ConnectionInfo: &persistancev1.DatabaseClaimConnectionInfo{
+							Username: "oldUser",
+						},
+					},
+				},
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DatabaseClaimReconciler{
+				Client: tt.reconciler.Client,
+				Log:    tt.reconciler.Log,
+				Scheme: tt.reconciler.Scheme,
+				Config: tt.reconciler.Config,
+			}
+			if got := r.isUserChanged(tt.args.dbClaim); got != tt.want {
+				t.Errorf("isUserChanged() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
