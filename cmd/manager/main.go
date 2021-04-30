df04d9e69f73d8946082ab2439236ff76ee04c3b
alavrovinfb <alavrov@infoblox.com>
2021-04-30 16:14:38 +0300

    adding health and readiness probes

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -19,18 +19,19 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/infobloxopen/db-controller/pkg/rdsauth"
 	"os"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	persistancev1 "github.com/infobloxopen/db-controller/api/v1"
 	"github.com/infobloxopen/db-controller/controllers"
 	"github.com/infobloxopen/db-controller/pkg/config"
+	"github.com/infobloxopen/db-controller/pkg/rdsauth"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -52,8 +53,12 @@ func main() {
 	var metricsPort int
 	var enableLeaderElection bool
 	var configFile string
+	var probeAddr string
+	var probePort int
 	flag.StringVar(&metricsAddr, "metrics-addr", "0.0.0.0", "The address the metric endpoint binds to.")
 	flag.IntVar(&metricsPort, "metrics-port", 8080, "The port the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-address", "", "The address the probe endpoint binds to.")
+	flag.IntVar(&probePort, "health-probe-port", 8081, "The port the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
@@ -65,11 +70,12 @@ func main() {
 	ctrl.SetLogger(logger)
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:             scheme,
-		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsAddr, metricsPort),
-		Port:               9443,
-		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "32151587.atlas.infoblox.com",
+		Scheme:                 scheme,
+		MetricsBindAddress:     fmt.Sprintf("%s:%d", metricsAddr, metricsPort),
+		HealthProbeBindAddress: fmt.Sprintf("%s:%d", probeAddr, probePort),
+		Port:                   9443,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "32151587.atlas.infoblox.com",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -87,6 +93,15 @@ func main() {
 		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
 
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
