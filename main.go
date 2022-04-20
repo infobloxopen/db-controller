b01e51b6694c56333557d3f92f3de0aa0c15ffa2
Daniel Garcia <dgarcia@infoblox.com>
2022-04-20 16:19:12 -0500

    port project to kubebuilder v3

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-
+Copyright 2022.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,21 +18,22 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
 	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	persistancev1 "github.com/infobloxopen/db-controller/api/v1"
 	"github.com/infobloxopen/db-controller/controllers"
-	"github.com/infobloxopen/db-controller/pkg/config"
-	"github.com/infobloxopen/db-controller/pkg/rdsauth"
-	// +kubebuilder:scaffold:imports
+	//+kubebuilder:scaffold:imports
 )
 
 var (
@@ -41,46 +42,36 @@ var (
 )
 
 func init() {
-	_ = clientgoscheme.AddToScheme(scheme)
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	_ = persistancev1.AddToScheme(scheme)
-	// +kubebuilder:scaffold:scheme
-
+	utilruntime.Must(persistancev1.AddToScheme(scheme))
+	//+kubebuilder:scaffold:scheme
 }
 
 func main() {
 	var metricsAddr string
-	var metricsPort int
 	var enableLeaderElection bool
-	var configFile string
 	var probeAddr string
-	var probePort int
-	flag.StringVar(&metricsAddr, "metrics-addr", "0.0.0.0", "The address the metric endpoint binds to.")
-	flag.IntVar(&metricsPort, "metrics-port", 8080, "The port the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-address", "", "The address the probe endpoint binds to.")
-	flag.IntVar(&probePort, "health-probe-port", 8081, "The port the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	flag.StringVar(&configFile, "config-file", "/etc/config/config.yaml",
-		"Database connection string to with root credentials.")
 	opts := zap.Options{
-		Development: false,
+		Development: true,
 	}
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
-	logger := zap.New(zap.UseFlagOptions(&opts))
-	ctlConfig := config.NewConfig(logger, configFile)
-	ctrl.SetLogger(logger)
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
-		MetricsBindAddress:     fmt.Sprintf("%s:%d", metricsAddr, metricsPort),
-		HealthProbeBindAddress: fmt.Sprintf("%s:%d", probeAddr, probePort),
+		MetricsBindAddress:     metricsAddr,
 		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "32151587.atlas.infoblox.com",
+		LeaderElectionID:       "01434111.atlas.infoblox.com",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -88,16 +79,13 @@ func main() {
 	}
 
 	if err = (&controllers.DatabaseClaimReconciler{
-		Client:     mgr.GetClient(),
-		Log:        ctrl.Log.WithName("controllers").WithName("DatabaseClaim"),
-		Scheme:     mgr.GetScheme(),
-		Config:     ctlConfig,
-		MasterAuth: rdsauth.NewMasterAuth(),
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "DatabaseClaim")
 		os.Exit(1)
 	}
-	// +kubebuilder:scaffold:builder
+	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
