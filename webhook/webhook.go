5ce2a4f92487a7e2e245f2dfa9c579395ff1f415
Vidyasagara Guntaka <visa77@gmail.com>
2022-06-15 19:44:21 -0700

    Sagar/dbproxy injector webhook (#42)
    * DB Proxy end to end  preliminary code
    
    * add support for db-secret-path annotation
    
    * copy webhook durectory to the workspace compilation
    
    * remove excessive logs.  sanitize sidear json.
    
    * commandline flag to control invocation of dbproxy webhook registration
    
    * Remove temporary certs
    
    * restore the original db-controller config
    
    * correct comments

---
+++ webhook/webhook.go
@@ -0,0 +1,115 @@
+package hook
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// DBProxyInjector annotates Pods
+type DBProxyInjector struct {
+	Name                 string
+	Client               client.Client
+	decoder              *admission.Decoder
+	DBProxySidecarConfig *Config
+}
+
+var (
+	dbProxyLog = ctrl.Log.WithName("dbproxy-controller")
+)
+
+type Config struct {
+	Containers []corev1.Container `json:"containers"`
+	Volumes    []corev1.Volume    `json:"volumes"`
+}
+
+func dbProxySideCardInjectionRequired(pod *corev1.Pod) (bool, string) {
+	dbSecretPath, ok := pod.Annotations["infoblox.com/db-secret-path"]
+
+	if !ok {
+		return false, ""
+	}
+
+	alreadyInjected, err := strconv.ParseBool(pod.Annotations["infoblox.com/dbproxy-injected"])
+
+	if err == nil && alreadyInjected {
+		dbProxyLog.Info("DB Proxy sidecar already injected: ", pod.Name, dbSecretPath)
+		return false, dbSecretPath
+	}
+
+	dbProxyLog.Info("DB Proxy sidecar Injection required: ", pod.Name, dbSecretPath)
+
+	return true, dbSecretPath
+}
+
+// DBProxyInjector adds an annotation to every incoming pods.
+func (dbpi *DBProxyInjector) Handle(ctx context.Context, req admission.Request) admission.Response {
+	//dbProxyLog.Info("Sidecar config", "parsed:", dbpi.DBProxySidecarConfig)
+	pod := &corev1.Pod{}
+
+	err := dbpi.decoder.Decode(req, pod)
+	if err != nil {
+		dbProxyLog.Info("Sdecar-Injector: cannot decode")
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	if pod.Annotations == nil {
+		pod.Annotations = map[string]string{}
+	}
+
+	shoudInjectDBProxy, dbSecretPath := dbProxySideCardInjectionRequired(pod)
+
+	if shoudInjectDBProxy {
+		dbProxyLog.Info("Injecting sidecar...")
+
+		f := func(c rune) bool {
+			return c == '/'
+		}
+
+		dbSecretFields := strings.FieldsFunc(dbSecretPath, f)
+
+		if len(dbSecretFields) == 2 {
+			dbSecretRef := dbSecretFields[0]
+			dbSecretItem := dbSecretFields[1]
+
+			dbpi.DBProxySidecarConfig.Volumes[0].Secret.SecretName = dbSecretRef
+			dbpi.DBProxySidecarConfig.Volumes[0].Secret.Items[0].Key = dbSecretItem
+
+			pod.Spec.Volumes = append(pod.Spec.Volumes, dbpi.DBProxySidecarConfig.Volumes...)
+			pod.Spec.Containers = append(pod.Spec.Containers, dbpi.DBProxySidecarConfig.Containers...)
+
+			pod.Annotations["infoblox.com/dbproxy-injected"] = "true"
+
+			dbProxyLog.Info("sidecar ontainer for ", dbpi.Name, " injected.", pod.Name, pod.APIVersion)
+		} else {
+			dbProxyLog.Error(nil, "could not parse db secret path", "infoblox.com/db-secret-path", dbSecretPath)
+		}
+	} else {
+		dbProxyLog.Info("DB Proxy sidecar not needed.", pod.Name, pod.APIVersion)
+	}
+
+	marshaledPod, err := json.Marshal(pod)
+
+	// dbProxyLog.Info("pod definition:", "pod", marshaledPod)
+
+	if err != nil {
+		dbProxyLog.Info("DB Proxy sidecar injection: cannot marshal")
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+
+	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
+}
+
+// DBProxyInjector implements admission.DecoderInjector.
+// InjectDecoder injects the decoder.
+func (dbpi *DBProxyInjector) InjectDecoder(d *admission.Decoder) error {
+	dbpi.decoder = d
+	return nil
+}
