b0a240231c87b34cce51a1d0687b74115bfd487e
Vidyasagara Guntaka <vguntaka@infoblox.com>
2022-06-23 09:12:39 -0700

    Sagar/dbproxy (#50)
    * Support for sslmode enabled in dbproxy

--- dbproxy/start-pgbouncer.sh
+++ dbproxy/start-pgbouncer.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -x
 
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout dbproxy-server.key -out dbproxy-server.crt -subj "/C=US/CN=dbproxy-server/ST=CA/L=Santa Clara/O=Infoblox/OU=Blox in a Box"
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout dbproxy-client.key -out dbproxy-client.crt -subj "/C=US/CN=dbproxy-client/ST=CA/L=Santa Clara/O=Infoblox/OU=Blox in a Box"
+
 pgbouncer -d -v pgbouncer.ini
