8273451feb63636d2c6b329ff1f0bd1cf6ec166f
Drew Wells <dwells@infoblox.com>
2024-07-01 16:39:48 -0500

    change leader id back

--- cmd/main.go
+++ cmd/main.go
@@ -166,7 +166,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "01434111.atlas.infoblox.com",
+		LeaderElectionID:       "32151587.atlas.infoblox.com",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
