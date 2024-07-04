df5cb5d49cebe33eb6788b94af263b674a376d78
Drew Wells <dwells@infoblox.com>
2024-07-04 13:45:03 -0500

    PTEUDO-1096 scaffolding of integration tests (#249)
    * PTEUDO-1096 scaffolding of integration tests
    
        - Calling make as much as possible so testing is simpler for devs
        - Basic test to prove that controller is up in the cluster
        - Added conditions to make that only specific clusters will be deployed into
    
    * dont vet integration tests while being rewritten

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -18,11 +18,34 @@ package e2e
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"testing"
+
+	crossplanerdsv1alpha1 "github.com/crossplane-contrib/provider-aws/apis/rds/v1alpha1"
+	persistancev1 "github.com/infobloxopen/db-controller/api/v1"
+	"github.com/infobloxopen/db-controller/test/utils"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
+
+var (
+	namespace string
+	k8sClient client.Client
+)
+
+func init() {
+	namespace = os.Getenv("NAMESPACE")
+}
+
+// FIXME: remove this and use namespace instead
+var class = ""
 
 // Run e2e tests using the Ginkgo runner.
 func TestE2E(t *testing.T) {
@@ -30,3 +53,43 @@ func TestE2E(t *testing.T) {
 	fmt.Fprintf(GinkgoWriter, "Starting migration suite\n")
 	RunSpecs(t, "e2e suite")
 }
+
+var _ = BeforeSuite(func() {
+
+	Expect(namespace).NotTo(Equal(""), "you must set the namespace")
+	class = namespace
+
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+
+	var err error
+
+	// Add all the schemas needed
+	err = persistancev1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = crossplanerdsv1alpha1.SchemeBuilder.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	k8sClient, err = client.New(config.GetConfigOrDie(), client.Options{})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	By("Pushing the operator images")
+	cmd := exec.Command("make", "push-images")
+	_, err = utils.Run(cmd)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+
+	By("Helm upgrading the manager")
+	cmd = exec.Command("make", "deploy")
+	_, err = utils.Run(cmd)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+
+})
+
+var _ = AfterSuite(func() {
+
+	By("Helm upgrading the manager")
+	cmd := exec.Command("make", "undeploy")
+	_, err := utils.Run(cmd)
+	Expect(err).NotTo(HaveOccurred())
+})
