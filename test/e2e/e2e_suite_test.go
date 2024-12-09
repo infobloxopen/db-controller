7f08ff320523fa38dafbe90e06a0023f90942060
bfabricio <fbottega@infoblox.com>
2024-12-08 21:01:05 -0300

    PTEUDO-1990: Fix unit test

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -165,7 +165,7 @@ var _ = AfterSuite(func() {
 			Name:      db,
 			Namespace: namespace,
 		}
-		
+
 		By("Checking DatabaseClaim: " + db)
 		if err := k8sClient.Get(ctx, nname, claim); err == nil {
 			if _, exists := claim.GetLabels()["persistance.atlas.infoblox.com/allow-deletion"]; !exists {
@@ -175,16 +175,15 @@ var _ = AfterSuite(func() {
 				})
 				Expect(k8sClient.Update(ctx, claim)).Should(Succeed())
 			}
-	
+
 			By("Deleting DatabaseClaim: " + db)
 			Expect(k8sClient.Delete(ctx, claim)).Should(Succeed())
-			
+
 			Eventually(func() bool {
 				return errors.IsNotFound(k8sClient.Get(ctx, nname, claim))
 			}).Should(BeTrue())
 		}
 	}
-	
 
 	inst := crossplaneaws.DBInstance{}
 	for _, db := range []string{dbinstance1, dbinstance1update} {
