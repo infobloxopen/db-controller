2ba17e16ee2e283dc46cf1775e0cea058db0b28b
Leandro Toledo <lrichard@infoblox.com>
2025-01-13 20:45:33 -0300

    DEVOPS-30643: adds pgbouncer config to set the role before running SQL commands.

--- dbproxy/pgbouncer/config.go
+++ dbproxy/pgbouncer/config.go
@@ -15,6 +15,7 @@ import (
 	"text/template"
 )
 
+// PGBouncerConfig represents a pgbouncer configuration.
 type PGBouncerConfig struct {
 	tmpl       *template.Template
 	configPath string
@@ -31,8 +32,10 @@ type PGBouncerConfig struct {
 	UserName    string
 	Password    string
 	SSLMode     string
+	RoleName    string
 }
 
+// String returns the DSN for the pgbouncer config.
 func (pgb *PGBouncerConfig) String() string {
 	pass := pgb.Password
 	// Redact all but last 4 characters of password
@@ -50,7 +53,7 @@ type Params struct {
 	OutPath   string
 }
 
-// GenerateConfig generates a pgbouncer config from a dsn
+// NewConfig generates a pgbouncer config from a dsn
 // The Port is the local port to bind to
 // passwordPath is used with old style DSN as db-controller
 // tends not to update the password in it
@@ -82,69 +85,6 @@ func NewConfig(p Params) (*PGBouncerConfig, error) {
 	return &cfg, nil
 }
 
-// FIXME: remove if not used, webhook is hardcoded to
-// uri-formatted dsn
-func parseOldDSN(content string, passwordPath string) (PGBouncerConfig, error) {
-	var cfg PGBouncerConfig
-	fields := strings.Split(string(content), " ")
-
-	f := func(c rune) bool {
-		return (c == '=')
-	}
-
-	m := make(map[string]*string)
-
-	for _, field := range fields {
-		fieldPair := strings.FieldsFunc(field, f)
-		fieldPair = strings.SplitN(field, "=", 2)
-		if len(fieldPair) == 2 {
-			unquotedString := strings.Trim(fieldPair[1], `'`)
-			m[fieldPair[0]] = &unquotedString
-		} else if len(fieldPair) == 1 {
-			m[fieldPair[0]] = nil
-		}
-	}
-
-	passwordContent, err := ioutil.ReadFile(passwordPath)
-	if err != nil {
-		return cfg, err
-	}
-
-	// FIXME: password in dsn never gets updated, read from password key for rotated password
-	password := string(passwordContent)
-	m["password"] = &password
-
-	if m["host"] == nil {
-		return cfg, errors.New("host value not found in db credential")
-	}
-
-	if m["port"] == nil {
-		return cfg, errors.New("port value not found in db credential")
-	}
-
-	if m["dbname"] == nil {
-		return cfg, errors.New("dbname value not found in db credential")
-	}
-
-	if m["user"] == nil {
-		return cfg, errors.New("user value not found in db credential")
-	}
-
-	if m["password"] == nil {
-		return cfg, errors.New("password value not found in db credential")
-	}
-
-	cfg.RemoteHost = *m["host"]
-	cfg.UserName = *m["user"]
-	remotePort, _ := strconv.Atoi(*m["port"])
-	cfg.RemotePort = int16(remotePort)
-	cfg.Password = *m["password"]
-	cfg.LocalDbName = *m["dbname"]
-	cfg.SSLMode = *m["sslmode"]
-
-	return cfg, nil
-}
-
 func parseURI(c *PGBouncerConfig, dsn string) error {
 
 	u, err := url.Parse(dsn)
@@ -155,6 +95,9 @@ func parseURI(c *PGBouncerConfig, dsn string) error {
 	if u.Scheme != "postgres" && u.Scheme != "postgresql" {
 		return fmt.Errorf("invalid_scheme: %s", u.Scheme)
 	}
+
+	// Remove the _<a|b> suffix from the username.
+	c.RoleName = strings.Split(u.User.Username(), "_")[0]
 
 	c.RemoteHost = u.Hostname()
 	c.UserName = u.User.Username()
@@ -198,6 +141,7 @@ client_tls_sslmode = prefer
 client_tls_key_file=dbproxy-client.key
 client_tls_cert_file=dbproxy-client.crt
 server_tls_sslmode = {{.SSLMode}}
+server_reset_query = SET ROLE {{.RoleName}}
 #server_tls_key_file=dbproxy-server.key
 #server_tls_cert_file=dbproxy-server.crt
 `
@@ -214,7 +158,6 @@ func (config *PGBouncerConfig) Write() error {
 		return err
 	}
 
-	//func (p *PGBouncerConfig) WritePGBouncerConfig(path string, config *PGBouncerConfig) error {
 	configFile, err := os.OpenFile(config.configPath, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
